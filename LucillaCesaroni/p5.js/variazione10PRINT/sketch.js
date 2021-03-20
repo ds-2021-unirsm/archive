ac0d9ee3d4cca9aeba15508c74e8a1e122d0fef6
lllcsr <76476647+LucillaCesaroni@users.noreply.github.com>
2021-03-20 10:56:15 +0100

    sketch.js variazione10PRINT

---
+++ LucillaCesaroni/p5.js/variazione10PRINT/sketch.js
@@ -0,0 +1,44 @@
+// Variazione 10PRINT
+// Lucilla Cesaroni
+
+let w, h;
+let x = 0;
+let y = 0;
+let size = 20; // grandezza 
+var noisex = 0;
+var noisey = 0;
+var destraSinistra; // sceglie se obliq a dx o sx
+
+function setup() {
+    createCanvas(w = windowWidth, h = windowHeight);
+    background(255);
+    colorMode(HSB);
+}
+
+function draw() {
+    destraSinistra = random(1); // barra a destra o sinistra?
+
+    // scelta se obliqua a dx o sx
+    if (destraSinistra < 0.5) {
+        strokeCap(PROJECT); // estremità estese
+        strokeWeight(noise(noisex, noisey) * 1.5); // leggermente piu sottile
+        stroke(noise(noisex, noisey) * 360, noise(noisex, noisey) * 100, noise(noisex, noisey) * 100); // tutti i valori (tonalita, saturazione, luminosità) dipendono dal noise
+        line(x, y, x + size, y + size); // piccolo
+    } else if (destraSinistra > 0.5) {
+        strokeCap(SQUARE);
+        strokeWeight(noise(noisex, noisey) * 5); // leggermente piu spesso
+        stroke(noise(noisex, noisey) * 360, noise(noisex, noisey) * 100, noise(noisex, noisey) * 100);
+        line(x, y + size, x + size, y); // grande
+    }
+
+    x += size; // scorro a dx la prossima barretta
+    noisex += 0.3; // più alto, più la variazione si vede
+
+    // andare a capo
+    if (x > w) {
+        x = 0;
+        y += size;
+        noisex = 0;
+        noisey += 0.3;
+    }
+}
