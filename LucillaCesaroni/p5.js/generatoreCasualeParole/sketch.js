5d7819483c6a79aa17c477dcb8f1d8362ac6f430
lllcsr <76476647+LucillaCesaroni@users.noreply.github.com>
2021-03-24 10:34:27 +0100

    aggiunta window resized sketch.js

--- LucillaCesaroni/p5.js/generatoreCasualeParole/sketch.js
+++ LucillaCesaroni/p5.js/generatoreCasualeParole/sketch.js
@@ -23,78 +23,86 @@ var voice = new p5.Speech(); // new P5.Speech object
 let mouse = false;
 
 function setup() {
+  c = createCanvas(w = windowWidth, h = windowHeight);
+  textAlign(CENTER, CENTER);
+  colorMode(HSB, 360, 100, 100);
+  background(10);
 
-    c = createCanvas(w = windowWidth, h = windowHeight);
-    textAlign(CENTER, CENTER);
-    colorMode(HSB, 360, 100, 100);
-    background(10);
+  //scrive la prima parola al centro
+  x = w / 2;
+  y = h / 2;
 
-    //scrive la prima parola al centro
-    x = w / 2;
-    y = h / 2;
-
-    scriviParola();
+  scriviParola();
 }
 
 function draw() {
 
-    x = random(0, w);
-    y = random(0, h);
+  x = random(0, w);
+  y = random(0, h);
 
-    // ogni 120 secondi scrivi una parola
-    if (frameCount % 120 == 0) {
-        push();
-        size = random(10, size + 20);
-        scriviParola(); // chiamo la funzione
-        pop();
-    }
+  // ogni 120 secondi scrivi una parola
+  if (frameCount % 120 == 0) {
+    push();
+    size = random(10, size + 20);
+    scriviParola(); // chiamo la funzione
+    pop();
+  }
 
-    // pulisci il background quando clicchi
-    if (mouse == true) {
-        background(10);
-    }
-    mouse = false;
+  // pulisci il background quando clicchi
+  if (mouse == true) {
+    background(10);
+  }
+  mouse = false;
 
 }
 
 //premi tasto screen del poster
 function mousePressed() {
-    mouse = true;
+  mouse = true;
 }
 
-//premi tasto screen del poster
+//premi tasto, screen del poster
 function keyPressed() {
-    saveCanvas(c, 'Poster', 'jpg');
+  saveCanvas(c, 'Poster', 'jpg');
 }
 
 // funzione che scrive la parola
 function scriviParola() {
 
-    //lunghezza della parola
-    lunghezza = Math.floor(random(min, max));
+  //lunghezza della parola
+  lunghezza = Math.floor(random(min, max));
 
-    var h = 360 * noise(colore + 205);
-    var s = 100; //saturazione
-    var b = 100;
+  var h = 360 * noise(colore + 205);
+  var s = 100; //saturazione
+  var b = 100;
 
-    fill(h, s, b);
-    textFont("Raleway", size);
-    textStyle(BOLD);
+  fill(h, s, b);
+  textFont("Raleway", size);
+  textStyle(BOLD);
 
-    //con cosa inizio?
-    randomParola = random(vocaleConsonante);
+  //con cosa inizio?
+  randomParola = random(vocaleConsonante);
 
-    //per la lunghezza metto vocali e consonanti > max 8 lettere perche ne inserisco due alternate..
-    for (let i = 0; i < lunghezza; i++) {
-        let vocale_estratta = random(vocali);
-        randomParola += vocale_estratta;
-        let consonante_estratta = random(consonanti);
-        randomParola += consonante_estratta;
-    }
-    text(randomParola, x, y);
+  //per la lunghezza metto vocali e consonanti > max 8 lettere perche ne inserisco due alternate..
+  for (let i = 0; i < lunghezza; i++) {
+    let vocale_estratta = random(vocali);
+    randomParola += vocale_estratta;
+    let consonante_estratta = random(consonanti);
+    randomParola += consonante_estratta;
+  }
+  text(randomParola, x, y);
 
-    colore += 0.2;
+  colore += 0.2;
+  
+  // modifica della voce
+  voice.setLang('it-IT');
+  voice.setPitch(2.0);
+  
+  voice.speak(randomParola);
 
-    voice.speak(randomParola);
+}
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);  
+  background(10);
 }
