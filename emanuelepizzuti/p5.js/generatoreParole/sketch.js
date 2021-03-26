b68672b75e8c8c312ab06d85010272cfed977283
Emanuele Pizzuti <76455312+emanuelepizzuti@users.noreply.github.com>
2021-03-26 01:16:16 +0100

    upload sketch generatoreParole

---
+++ emanuelepizzuti/p5.js/generatoreParole/sketch.js
@@ -0,0 +1,63 @@
+// GENERATORE DI PAROLE RANDOM
+// sketch by emanuelepizzuti
+
+// Guida:
+// [mouse] click per generare una nuova parola
+
+var w, h;
+let iniziale = ["a", "e", "i", "o", "u"];
+let primaSillaba = ["ba", "be", "bi", "bo", "bu", "pa", "pe", "pi", "po", "pu"];
+let secSillaba = ["bb", "cc", "dd", "ff", "gg", "ll", "mm", "rr", "zz"];
+let randomParola = "";
+var x;
+var y;
+let mouse;
+
+// lunghezza della parola
+// let min = 1;
+// let max = 2;
+// let lunghezza;
+
+var voice = new p5.Speech(); // new P5.Speech object
+
+function setup() {
+  createCanvas(w = windowWidth, h = windowHeight);
+  background(235);
+
+  // noLoop();
+  // y = 40;
+  // x = 20;
+
+  mouse = false;
+}
+
+function draw() {
+  textAlign(CENTER, CENTER);
+
+  fill(200);
+  textFont("monospace", 36);
+  text("[CLICK to create a new word!]", width / 2, 40);
+
+  textFont("monospace", 72);
+  //lunghezza della parola
+  lunghezza = Math.floor(random(min, max));
+
+  if (mouse == true) {
+    background(235);
+    fill(20);
+    //parola
+    randomParola = random(iniziale) + random(primaSillaba) + random(secSillaba) + random(iniziale);
+    text(randomParola, w / 2, h / 2);
+
+    voice.setPitch(0.5)
+    voice.setLang('it-IT');
+    voice.speak(randomParola);
+  }
+
+  mouse = false;
+
+}
+
+function mousePressed() {
+  mouse = true;
+}
