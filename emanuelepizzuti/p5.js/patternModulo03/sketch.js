8af5e04e73c25f5804f83d864fd16ecf3252a338
Emanuele Pizzuti <76455312+emanuelepizzuti@users.noreply.github.com>
2021-05-05 12:52:50 +0200

    creo sketch patternmodulo03

---
+++ emanuelepizzuti/p5.js/patternModulo03/sketch.js
@@ -0,0 +1,44 @@
+// sketch by @emanuelepizzuti
+
+let distancePoint;
+let shapeAngle = 0;
+let w, h;
+let t, s;
+
+function setup() {
+  createCanvas(w = windowWidth, h = windowHeight);
+  noStroke();
+  distancePoint = dist(120, 120, width, height);
+  // colorMode(HSB);
+}
+
+function draw() {
+  blendMode(BLEND);
+  background(25);
+  blendMode(ADD);
+  
+  t=0;
+  s=0;
+
+  for (var posX = w / 2 - 400; posX < w / 2 + 400; posX += 60) {
+    for (var posY = h / 2 - 400; posY < h / 2 + 400; posY += 60) {
+      
+      fill(noise(t, s)*posX/2, noise(t, s)*50, noise(t, s)*posY/2);
+
+      // calculate angle between mouse position and actual position of the shape
+      var angle = atan2(mouseY - posY, mouseX - posX) + (shapeAngle * (PI / 180));
+      let size = dist(mouseX, mouseY, posX, posY);
+      size = size / distancePoint * 40;
+
+      push();
+      translate(posX, posY);
+      rotate(angle);
+      
+      triangle(-10, -15, -10, 15, frameCount % 360, 0)
+      pop();
+      
+      t+=0.5
+      s+=0.5
+    }
+  }
+}
