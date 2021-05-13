f8dd44875a26861081a9b403a08a80bba009c2c7
Lucrezia604 <79698027+Lucrezia604@users.noreply.github.com>
2021-05-13 15:41:59 +0200

    Add files via upload

---
+++ Lucrezia604/INVISIBILE/Prototipi/Test_01/riproduttore Giphy/sketch.js
@@ -0,0 +1,41 @@
+// Riproduttore di gif animate [Giphy]
+// Vengono riprodotte cliccando sullo schermo fino ad un massimo di 10
+// Laboratorio di Design dei Sistemi Interattivi, a.a. 2020/2021, prof. Daniele Tabellini, Design UNIRSM
+
+
+//url api sito giphy
+let apiUrl = "https://api.giphy.com/v1/gifs/random?api_key=2BXIaO9m3yFL15yhQV2LkXuUydxTdBep";
+let cnt = 0;   //contatore numero di GIF stampate
+ 
+function setup() { 
+  noCanvas();   //rimuove il canvas di default
+  createP("Clicca per un GIF casuale");   //scrive sullo schermo
+} 
+
+//ogni volta che clicco con il mouse
+function mousePressed() {
+  //se ho cliccato meno di 10 volte
+  if(cnt < 10){
+    loadJSON(apiUrl, giphyLoaded);    //carico una GIF
+  }
+
+}
+
+function giphyLoaded(respObj) {
+  cnt++;   //aumento il contatore
+  console.log("in loaded", respObj);   //scrivo in console
+  let imgsrc = respObj.data.image_original_url;   // recupero l'url della GIF
+  removeElements();   //rimuovo eventuali elementi precedenti
+  createImg(imgsrc);  //creo l'immagine 
+  
+  //se cnt è minore di 10 può chiedere un'altra GIF
+  if(cnt <10)
+    createP("Clicca per una GIF casuale");  
+  
+  //se cnt = 10 gli dico stop
+  if(cnt == 10)
+  {
+    createP("stop"); 
+    cnt++;
+  } 
+}
