44579c2d7f3d6b3e461780ccad169bc715ed8136
Lucilla <lucillacesaroni@gmail.com>
2021-05-20 20:36:18 +0200

    comment sketch.js

--- LucillaCesaroni/p5.js/fotoPixel/sketch.js
+++ LucillaCesaroni/p5.js/fotoPixel/sketch.js
@@ -1,24 +1,43 @@
+// -
+//
+// Foto pixel by Lucilla Cesaroni [pixel, mouseX, mouseY, keys]
+// 2021 Lucilla @LucillaCesaroni, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm - github.com/fupete - github.com/LucillaCesaroni 
+// Educational purposes, MIT License, 2021, San Marino
+//
+// —
+//
+// Credits/Thanks to: 
 // Inspired by Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
 //
-// foto pixel
+// http://www.generative-gestaltung.de
 //
-// Lucilla Cesaroni
-
-/**
- * extract and sort the color palette of an image
- *
- * MOUSE
- * position x          : resolution
- *
- * KEYS
- * 1                   : null
- * 2                   : sort colors on hue
- * 3                   : sort colors on saturation
- * 4                   : sort colors on brightness
- * 5                   : sort colors on greyscale (luminance)
- * s                   : save png
- * c                   : save color palette
- */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// —
+//
+// Help:
+// [1] null
+// [2] sort colors on hue
+// [3] sort colors on saturation
+// [4] sort colors on brightness
+// [5] sort colors on greyscale (luminance)
+// [s] save png
+// [c] save color palette
+// [mouseX] resolution
+//
+// —
 
 var img;
 var colors = [];
