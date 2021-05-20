4806820a0b7330ba992a2ea382665e6b08ed33db
Lucilla <lucillacesaroni@gmail.com>
2021-05-20 21:43:00 +0200

    commenti sketch.js

--- LucillaCesaroni/p5.js/incontriScontriCamminatori/sketch.js
+++ LucillaCesaroni/p5.js/incontriScontriCamminatori/sketch.js
@@ -1,6 +1,6 @@
 // -
 //
-// Incontri/scontri tra camminatori che cambiano la via by Lucilla Cesaroni [keyword1, keyword2]
+// Incontri/scontri tra camminatori che cambiano la via by Lucilla Cesaroni [bouncing]
 // 2021 Lucilla @LucillaCesaroni, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
 // github.com/ds-2021-unirsm - github.com/fupete - github.com/LucillaCesaroni 
 // Educational purposes, MIT License, 2021, San Marino
