430f1334667bc74f0627fc3f73870cf289ca509c
RobertoAlesi <76455356+RobertoAlesi@users.noreply.github.com>
2021-05-24 22:32:46 +0200

    commento iniziale

--- RobertoAlesi/P5.js/Variazioni_10Print/02. Ragnatele/sketch.js
+++ RobertoAlesi/P5.js/Variazioni_10Print/02. Ragnatele/sketch.js
@@ -1,3 +1,15 @@
+//  ______  ____  ___  _______  __________ ______ ____ ______________
+//  __/ _ \/ __ \/ _ )/ __/ _ \/_  __/ __ \ _/ _ |_/ /__/ __/ __/  _/
+//  _/ , _/ /_/ / _  / _// , _/ / / / /_/ / / __ |/ /__/ _/_\ \_/ /
+//  /_/|_|\____/____/___/_/|_| /_/  \____/ /_/ |_/____/___/___/___/
+//
+// -
+// Generatore_ragnatele 0.1 by Roberto [generatore, ragnatele, 10print]
+// 2021 © Roberto @ciunart, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete — github.com/RobertoAlesi
+// Educational purposes, MIT License, 2021, San Marino
+// —
+
 var w, h;
 var x = 0;
 var y = 0;
