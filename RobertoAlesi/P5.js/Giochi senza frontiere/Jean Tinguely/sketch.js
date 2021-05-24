32ca4b56b54a1d68767785eaaece5beb503bab54
RobertoAlesi <76455356+RobertoAlesi@users.noreply.github.com>
2021-05-24 22:25:53 +0200

    commento iniziale

--- RobertoAlesi/P5.js/Giochi senza frontiere/Jean Tinguely/sketch.js
+++ RobertoAlesi/P5.js/Giochi senza frontiere/Jean Tinguely/sketch.js
@@ -1,3 +1,15 @@
+//  ______  ____  ___  _______  __________ ______ ____ ______________
+//  __/ _ \/ __ \/ _ )/ __/ _ \/_  __/ __ \ _/ _ |_/ /__/ __/ __/  _/
+//  _/ , _/ /_/ / _  / _// , _/ / / / /_/ / / __ |/ /__/ _/_\ \_/ /
+//  /_/|_|\____/____/___/_/|_| /_/  \____/ /_/ |_/____/___/___/___/
+//
+// -
+// Jean_Tinguely 0.1 by Roberto [jean, tinguely]
+// 2021 © Roberto @ciunart, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete — github.com/RobertoAlesi
+// Educational purposes, MIT License, 2021, San Marino
+// —
+
 var r = 0;
 
 function setup() {
