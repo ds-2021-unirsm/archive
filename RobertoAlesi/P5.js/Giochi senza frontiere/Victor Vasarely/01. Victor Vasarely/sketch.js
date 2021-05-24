9aa3f55d518729b2cae7cb083e494c29a94ddcb8
RobertoAlesi <76455356+RobertoAlesi@users.noreply.github.com>
2021-05-24 22:26:27 +0200

    commento iniziale

--- RobertoAlesi/P5.js/Giochi senza frontiere/Victor Vasarely/01. Victor Vasarely/sketch.js
+++ RobertoAlesi/P5.js/Giochi senza frontiere/Victor Vasarely/01. Victor Vasarely/sketch.js
@@ -1,4 +1,16 @@
-  function setup() {
+//  ______  ____  ___  _______  __________ ______ ____ ______________
+//  __/ _ \/ __ \/ _ )/ __/ _ \/_  __/ __ \ _/ _ |_/ /__/ __/ __/  _/
+//  _/ , _/ /_/ / _  / _// , _/ / / / /_/ / / __ |/ /__/ _/_\ \_/ /
+//  /_/|_|\____/____/___/_/|_| /_/  \____/ /_/ |_/____/___/___/___/
+//
+// -
+// Victor_Vasarely 0.1 by Roberto [victor, vasarely]
+// 2021 © Roberto @ciunart, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete — github.com/RobertoAlesi
+// Educational purposes, MIT License, 2021, San Marino
+// —   
+
+function setup() {
       createCanvas(400, 400);
       rectMode(CENTER);
   }
