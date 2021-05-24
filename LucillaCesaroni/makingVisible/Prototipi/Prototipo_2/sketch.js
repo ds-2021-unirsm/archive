7cff40dd379382a28bc26e5fac66bba9e7aa1037
Lucilla <lucillacesaroni@gmail.com>
2021-05-24 02:06:10 +0200

    sketch.js

--- LucillaCesaroni/makingVisible/Prototipi/Prototipo_2/sketch.js
+++ LucillaCesaroni/makingVisible/Prototipi/Prototipo_2/sketch.js
@@ -1,8 +1,8 @@
 // -
 //
 // XXX by Lucilla Cesaroni [translations, images, words]
-// 2021 Lucilla @LucillaCesaroni, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
-// github.com/ds-2021-unirsm - github.com/fupete - github.com/LucillaCesaroni 
+// 2021 Lucilla @LucillaCesaroni, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm
+// github.com/ds-2021-unirsm - github.com/fupete - github.com/LucillaCesaroni
 // Educational purposes, MIT License, 2021, San Marino
 //
 // -
@@ -26,7 +26,7 @@
 // Photos from Unsplash for
 // https://unsplash.com
 //
-// Sentiment Analysis  
+// Sentiment Analysis
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -40,7 +40,6 @@
 // [click] buttons for activate start, pause, stop, new
 //
 // —
-
 
 let faceapi;
 let video;
@@ -56,6 +55,8 @@ let token = "436c5baef9e54af1a0de6a370e4ade38";
 // Immagini
 let immagini = []; // array immagini da visualizzare
 var imgsResolution = "640x480"; // risoluzione immagini
+let wImg = 150 / 2;
+let hImg = 100 / 2;
 
 // Speech Object
 let speechRec;
@@ -99,7 +100,7 @@ const detection_options = {
 function setup() {
   createCanvas(360 * 2, 270 * 2);
 
-  textFont(myFont, 20);
+  textFont(myFont, 15);
 
   colorMode(HSB, 360, 100, 100);
 
@@ -120,27 +121,102 @@ function modelReady() {
   faceapi.detect(gotResults);
 
   // bottoni
-  speakbutton = createButton("▶️");
+  speakbutton = createButton("start");
+  speakbutton.style("background-color", "#ffffff");
+  speakbutton.style("color", "#000000");
+  speakbutton.style("border", "1px solid #000000");
+  speakbutton.style("font-family", "Inter-Regular");
+  speakbutton.style("font-size", "1em");
+  speakbutton.style("padding-top", "6px");
+  speakbutton.style("padding-bottom", "6px");
+  speakbutton.style("padding-left", "15px");
+  speakbutton.style("padding-right", "15px");
+  speakbutton.mouseOver(onTop0).mouseOut(outside0);
   speakbutton.mousePressed(startRec);
   speakbutton.position(10, 600);
 
-  pausebutton = createButton("⏸️");
+  pausebutton = createButton("pause");
+  pausebutton.style("background-color", "#ffffff");
+  pausebutton.style("color", "#000000");
+  pausebutton.style("border", "1px solid #000000");
+  pausebutton.style("font-family", "Inter-Regular");
+  pausebutton.style("font-size", "1em");
+  pausebutton.style("padding-top", "6px");
+  pausebutton.style("padding-bottom", "6px");
+  pausebutton.style("padding-left", "15px");
+  pausebutton.style("padding-right", "15px");
+  pausebutton.mouseOver(onTop1).mouseOut(outside1);
   pausebutton.mousePressed(pauseRec);
-  pausebutton.position(50, 600);
+  pausebutton.position(80, 600);
 
-  stopspeakbutton = createButton("⏹️");
+  stopspeakbutton = createButton("stop");
+  stopspeakbutton.style("background-color", "#ffffff");
+  stopspeakbutton.style("color", "#000000");
+  stopspeakbutton.style("border", "1px solid #000000");
+  stopspeakbutton.style("font-family", "Inter-Regular");
+  stopspeakbutton.style("font-size", "1em");
+  stopspeakbutton.style("padding-top", "6px");
+  stopspeakbutton.style("padding-bottom", "6px");
+  stopspeakbutton.style("padding-left", "15px");
+  stopspeakbutton.style("padding-right", "15px");
+  stopspeakbutton.mouseOver(onTop2).mouseOut(outside2);
   stopspeakbutton.mousePressed(stopRec);
-  stopspeakbutton.position(90, 600);
+  stopspeakbutton.position(160, 600);
 
-  cancellaspeakbutton = createButton("🆕");
+  cancellaspeakbutton = createButton("new");
+  cancellaspeakbutton.style("background-color", "#ffffff");
+  cancellaspeakbutton.style("color", "#000000");
+  cancellaspeakbutton.style("border", "1px solid #000000");
+  cancellaspeakbutton.style("font-family", "Inter-Regular");
+  cancellaspeakbutton.style("font-size", "1em");
+  cancellaspeakbutton.style("padding-top", "6px");
+  cancellaspeakbutton.style("padding-bottom", "6px");
+  cancellaspeakbutton.style("padding-left", "15px");
+  cancellaspeakbutton.style("padding-right", "15px");
+  cancellaspeakbutton.mouseOver(onTop3).mouseOut(outside3);
   cancellaspeakbutton.mousePressed(newRec);
-  cancellaspeakbutton.position(130, 600);
+  cancellaspeakbutton.position(228, 600);
+
+  function onTop0() {
+    speakbutton.style("background-color", "#000000");
+    speakbutton.style("color", "#ffffff");
+  }
+  function outside0() {
+    speakbutton.style("background-color", "#ffffff");
+    speakbutton.style("color", "#000000");
+  }
+
+  function onTop1() {
+    pausebutton.style("background-color", "#000000");
+    pausebutton.style("color", "#ffffff");
+  }
+  function outside1() {
+    pausebutton.style("background-color", "#ffffff");
+    pausebutton.style("color", "#000000");
+  }
+
+  function onTop2() {
+    stopspeakbutton.style("background-color", "#000000");
+    stopspeakbutton.style("color", "#ffffff");
+  }
+  function outside2() {
+    stopspeakbutton.style("background-color", "#ffffff");
+    stopspeakbutton.style("color", "#000000");
+  }
+
+  function onTop3() {
+    cancellaspeakbutton.style("background-color", "#000000");
+    cancellaspeakbutton.style("color", "#ffffff");
+  }
+  function outside3() {
+    cancellaspeakbutton.style("background-color", "#ffffff");
+    cancellaspeakbutton.style("color", "#000000");
+  }
 }
 
 function startRec() {
   recordingAvviato = true;
-  console.log("Hai cliccato raccontami");
-  console.log("Sono qua, inizia a raccontare...");
+  console.log("Hai cliccato raccontami, inizia a raccontare...");
 
   // Crea un'oggetto Speech Recognition con una callback
   speechRec = new p5.SpeechRec("it-IT", gotSpeech);
@@ -172,7 +248,10 @@ function pauseRec() {
 function stopRec() {
   console.log("Hai cliccato stop rec");
 
+  contatoreEntita = 0;
+
   recordingAvviato = false;
+
   // Se hai stoppato la registrazione parte l'analisi del sentiment
   if (recordingAvviato == false) {
     // Parte l'analisi x le entità del racconto
@@ -187,12 +266,15 @@ function stopRec() {
   }
 }
 
-function newRec(){
-    console.log("Hai cliccato cancella tutto");
+function newRec() {
+  console.log("Hai cliccato cancella tutto");
 
-    recordingAvviato = false;
-    racconto = "";
-    immagini = [];
+  recordingAvviato = false;
+  tuttoProntoNoEntita = false;
+  tuttoPronto = false;
+  racconto = "";
+  immagini = [];
+  contatoreEntita = 0;
 }
 
 // Immagini da unsplash
@@ -208,7 +290,7 @@ function visualizzaRisposta(risposta) {
 
     noStroke();
     fill(0);
-    text(racconto, 40, 500, 620);
+    text(racconto, 130, 250, 180);
     return;
   }
 
@@ -251,8 +333,6 @@ function salvaimmagine(img) {
   }
 }
 
-function draw() {}
-
 function gotResults(err, result) {
   if (err) {
     console.log(err);
@@ -280,21 +360,21 @@ function gotResults(err, result) {
     //console.log(keys);
     keys.forEach((item, idx) => {
       // scritta
-      fill(0);
+      fill(255);
       noStroke();
-      textSize(20);
-      text(`${item}`, 40, idx * 20 + 20);
+      textSize(15);
+      text(`${item}`, 40, idx * 23 + 40);
 
       // funzione che prende il valore
       val[idx] = expressions[item];
-      let valore = map(expressions[item], 0, 1, 0, width / 2);
-      fill(0);
+      let valore = map(expressions[item], 0, 1, 0, width / 4);
+      fill(255);
       noStroke();
-      rect(160, idx * 20 + 14, valore, 6);
-      stroke(0);
+      rect(130, idx * 23 + 32, valore, 6); // x y w h
+      stroke(255);
       strokeWeight(1);
       noFill();
-      rect(160, idx * 20 + 14, width / 2, 6);
+      rect(130, idx * 23 + 32, width / 4, 6);
     });
 
     valoreMassimo = max(val);
@@ -329,14 +409,14 @@ function gotResults(err, result) {
 
     if (detections.length > 0) {
       // console.log(detections)
-      drawBox(detections);
+      //drawBox(detections);
       drawLandmarks(detections);
     }
   }
   faceapi.detect(gotResults);
 }
 
-function drawBox(detections) {
+/*function drawBox(detections) {
   for (let i = 0; i < detections.length; i++) {
     const alignedRect = detections[i].alignedRect;
     const x = alignedRect._box._x;
@@ -346,53 +426,63 @@ function drawBox(detections) {
 
     noFill();
     stroke(161, 95, 251);
-    strokeWeight(2);
+    strokeWeight(1);
     rect(x, y, boxWidth, boxHeight);
   }
 }
-
+*/
 function drawLandmarks(detections) {
-  noFill();
-  stroke(161, 95, 251);
-  strokeWeight(2);
-
-  let x = -75;
-  let y = 0;
+  //console.log("detections: "+ JSON.stringify(detections))
 
   for (let i = 0; i < detections.length; i++) {
-    const mouth = detections[i].parts.mouth;
-    const nose = detections[i].parts.nose;
-    const leftEye = detections[i].parts.leftEye;
-    const rightEye = detections[i].parts.rightEye;
-    const rightEyeBrow = detections[i].parts.rightEyeBrow;
-    const leftEyeBrow = detections[i].parts.leftEyeBrow;
+    // me lo continuerà a mandare all'infinito
 
-    drawPart(mouth, true);
-    drawPart(nose, false);
-    drawPart(leftEye, true);
-    drawPart(leftEyeBrow, false);
-    drawPart(rightEye, true);
-    drawPart(rightEyeBrow, false);
+    let positions = []; // creo un'array per le posizioni dei 6 punti
+
+    positions[0] = detections[i].parts.leftEyeBrow;
+    positions[1] = detections[i].parts.rightEyeBrow;
+    positions[2] = detections[i].parts.mouth;
+    positions[3] = detections[i].parts.nose;
+    positions[4] = detections[i].parts.leftEye;
+    positions[5] = detections[i].parts.rightEye;
 
     if (tuttoProntoNoEntita == true) {
       //console.log("eccomi");
       noStroke();
       fill(sentiment, 100, 100);
-      text(racconto, 40, 500, 620);
+      text(racconto, 130, 250, 180);
     } else if (tuttoPronto == true) {
+      // console.log ("sto disegnando le img");
       noStroke();
       fill(sentiment, 100, 100);
-      text(racconto, 40, 500, 620);
+      text(racconto, 130, 250, 180);
 
       for (let i = 0; i < immagini.length; i++) {
-        image(immagini[i], mouth[3]._x + x, mouth[3]._y + y, 150, 100);
-        y += 100;
+        // mi passo l'array delle immagini
+        drawPart(positions[i % 6], i); // ogni 6 ricomincia il giro perche 0 modulo 6 fa 0 e 6 modulo 6 fa 0 di nuovo
       }
     }
   }
+  function drawPart(feature, num) {
+    // ellipse(feature[3]._x, feature[3]._y, 10, 10);
+    let x = 0;
+    let y = 0;
 
-  function drawPart(feature, closed) {
-    ellipse(feature[3]._x, feature[3]._y, 10, 10);
+    // se le entita superano 6
+    if (num >= 6) {
+      x = -30;
+      y = 30;
+    }
+
+    // console.log("feature: " + JSON.stringify(feature));
+
+    image(
+      immagini[num],
+      feature[3]._x + x - wImg / 2,
+      feature[3]._y + y - hImg / 2,
+      wImg,
+      hImg
+    );
   }
 }
 /*
