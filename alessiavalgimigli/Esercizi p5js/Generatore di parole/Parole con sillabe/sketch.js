f6f66a6e5b8de692bed9d85848274bcf8d9c9524
alessiavalgimigli <79698172+alessiavalgimigli@users.noreply.github.com>
2021-05-25 21:24:53 +0100

    commento inizio

--- alessiavalgimigli/Esercizi p5js/Generatore di parole/Parole con sillabe/sketch.js
+++ alessiavalgimigli/Esercizi p5js/Generatore di parole/Parole con sillabe/sketch.js
@@ -1,4 +1,11 @@
- let sillabe = ["ba", "be", "bi", "bo", "bu", "ca", "ce", "ci", "co", "cu", "cra", "cre", "cri", "cro", "cru", "che", "chi", "da", "de", "di", "do", "du", "fa", "fe", "fi", "fo", "fu", "ga", "ge", "gi", "go", "gu", "ghe", "ghi", "gra", "gre", "gri", "gro", "gru", "la", "le", "li", "lo", "lu", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "pa", "pe", "pi", "po", "pu", "pra", "pre", "pri", "pro", "pru", "qu", "ra", "re", "ri", "ro", "ru", "sa", "se", "si", "so", "su", "ta", "te", "ti", "to", "tu", "tra", "tre", "tri", "tro", "tru", "va", "ve", "vi", "vo", "vu", "za", "ze", "zi", "zo", "zu",  ];
+// -
+// Generatore di parole con sillabe 0.1 by Alessia Valgimigli [generatore, parole]
+// 2021 © Alessia Valgimigli, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete
+// Educational purposes, MIT License, 2021, San Marino
+// —
+
+let sillabe = ["ba", "be", "bi", "bo", "bu", "ca", "ce", "ci", "co", "cu", "cra", "cre", "cri", "cro", "cru", "che", "chi", "da", "de", "di", "do", "du", "fa", "fe", "fi", "fo", "fu", "ga", "ge", "gi", "go", "gu", "ghe", "ghi", "gra", "gre", "gri", "gro", "gru", "la", "le", "li", "lo", "lu", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "pa", "pe", "pi", "po", "pu", "pra", "pre", "pri", "pro", "pru", "qu", "ra", "re", "ri", "ro", "ru", "sa", "se", "si", "so", "su", "ta", "te", "ti", "to", "tu", "tra", "tre", "tri", "tro", "tru", "va", "ve", "vi", "vo", "vu", "za", "ze", "zi", "zo", "zu",  ];
 
 let parola=[];
 
