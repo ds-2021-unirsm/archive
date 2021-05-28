ae6f37afc2c471b690079e56c871e094c8d75ecb
RobertoAlesi <76455356+RobertoAlesi@users.noreply.github.com>
2021-05-28 10:55:37 +0200

    Update sketch.js

--- RobertoAlesi/Making_Visible/Sperimentazioni/HandPose 3D Control/sketch.js
+++ RobertoAlesi/Making_Visible/Sperimentazioni/HandPose 3D Control/sketch.js
@@ -1,3 +1,22 @@
+//  ______  ____  ___  
+//  __/ _ \/ __ \/ _ )
+//  _/ , _/ /_/ / _  / 
+//  /_/|_|\____/____/ 
+//
+// -
+// HandPose_3D 0.1 by Roberto [hand, pose, cube, sphere, 3d, nointerface]
+// 2021 © Roberto @ciunart, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm
+// github.com/ds-2021-unirsm — github.com/fupete — github.com/RobertoAlesi
+// Educational purposes, MIT License, 2021, San Marino
+// —
+// 
+// Hand gesture:
+// [rock] -> cambia forma in cubo
+// [aperta] -> cambia forma in sfera
+// [chiusa] -> cambia colore
+// [pinch] -> controlla la dimensione
+// —
+
 let handpose;
 let video;
 let predictions = [];
