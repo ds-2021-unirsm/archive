7288e552edc698342f1e3f30aaee82d14bf432ad
Emanuele Pizzuti <76455312+emanuelepizzuti@users.noreply.github.com>
2021-06-04 18:11:54 +0200

    Update sketch.js

--- emanuelepizzuti/p5.js/camminatori/camminatori03/sketch.js
+++ emanuelepizzuti/p5.js/camminatori/camminatori03/sketch.js
@@ -1,4 +1,15 @@
-// random walker [lines connected] OOP
+//  ____ ____
+// | ___|  _ |
+// | ___|  __|
+// |____|_|
+// ___________
+//
+// walkingRectangles by emanuelepizzuti [walkers, perling noise, OOP]
+// 2021 © emanuelepizzuti, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete
+// Educational purposes, MIT License, 2021, San Marino
+// ___________
+
 var w, h;
 var quanti = 10; // vertici
 var c = [];
