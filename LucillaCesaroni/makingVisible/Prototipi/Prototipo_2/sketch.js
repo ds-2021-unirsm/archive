f44049661841defae285a73852e6dcf3dc44fd9c
Lucilla <lucillacesaroni@gmail.com>
2021-06-05 18:11:23 +0200

    update sketch.js

--- LucillaCesaroni/makingVisible/Prototipi/Prototipo_2/sketch.js
+++ LucillaCesaroni/makingVisible/Prototipi/Prototipo_2/sketch.js
@@ -121,149 +121,31 @@ const detection_options = {
 };
 
 function setup() {
-  createCanvas(windowWidth - 30, windowHeight); // guarda https://github.com/processing/p5.js-web-editor/issues/680
+  createCanvas(windowWidth-20 , windowHeight-20); // guarda https://github.com/processing/p5.js-web-editor/issues/680 || per i bottoni
 
   textFont(myFont, 15);
 
-  colorMode(HSB, 360, 100, 100);
+  background(255)
 
-  // Elementi utili per sentiment analysis volto
-  paragraph = createP(); // Creates a <p></p> element
-  paragraph.style("font-size", 100 + "px");
+  colorMode(HSB, 360, 100, 100);
 
   // load up your video
   video = createCapture(VIDEO);
-
+  
   video.hide(); // Hide the video element, and just show the canvas
 
-  origineX = windowWidth / 2 - video.width;
-  origineY = windowHeight / 2 - video.height;
+  origineX = windowWidth / 2 - video.width - 35;
+  origineY = windowHeight / 2 - video.height - 90; // - altezza del rettangolo sottotitoli
 
   faceapi = ml5.faceApi(video, detection_options, modelReady);
+
+  disegnapulsanti();
 }
 
 function modelReady() {
   console.log("ready!");
   console.log(faceapi);
   faceapi.detect(gotResults);
-
-  // bottoni
-  speakbutton = createButton("start");
-  speakbutton.style("background-color", "#ffffff");
-  speakbutton.style("color", "#000000");
-  speakbutton.style("border", "1px solid #000000");
-  speakbutton.style("font-family", "Inter-Regular");
-  speakbutton.style("font-size", "1em");
-  speakbutton.style("padding-top", "6px");
-  speakbutton.style("padding-bottom", "6px");
-  speakbutton.style("padding-left", "15px");
-  speakbutton.style("padding-right", "15px");
-  speakbutton.mouseOver(inside0).mouseOut(outside0);
-  speakbutton.mousePressed(startRec);
-  speakbutton.position(origineX + 5, windowHeight / 1.37);
-
-  pausebutton = createButton("pause");
-  pausebutton.style("background-color", "#ffffff");
-  pausebutton.style("color", "#000000");
-  pausebutton.style("border", "1px solid #000000");
-  pausebutton.style("font-family", "Inter-Regular");
-  pausebutton.style("font-size", "1em");
-  pausebutton.style("padding-top", "6px");
-  pausebutton.style("padding-bottom", "6px");
-  pausebutton.style("padding-left", "15px");
-  pausebutton.style("padding-right", "15px");
-  pausebutton.mouseOver(inside1).mouseOut(outside1);
-  pausebutton.mousePressed(pauseRec);
-  pausebutton.position(origineX + 75, windowHeight / 1.37);
-
-  stopspeakbutton = createButton("stop");
-  stopspeakbutton.style("background-color", "#ffffff");
-  stopspeakbutton.style("color", "#000000");
-  stopspeakbutton.style("border", "1px solid #000000");
-  stopspeakbutton.style("font-family", "Inter-Regular");
-  stopspeakbutton.style("font-size", "1em");
-  stopspeakbutton.style("padding-top", "6px");
-  stopspeakbutton.style("padding-bottom", "6px");
-  stopspeakbutton.style("padding-left", "15px");
-  stopspeakbutton.style("padding-right", "15px");
-  stopspeakbutton.mouseOver(inside2).mouseOut(outside2);
-  stopspeakbutton.mousePressed(stopRec);
-  stopspeakbutton.position(origineX + 155, windowHeight / 1.37);
-
-  cancellaspeakbutton = createButton("new");
-  cancellaspeakbutton.style("background-color", "#ffffff");
-  cancellaspeakbutton.style("color", "#000000");
-  cancellaspeakbutton.style("border", "1px solid #000000");
-  cancellaspeakbutton.style("font-family", "Inter-Regular");
-  cancellaspeakbutton.style("font-size", "1em");
-  cancellaspeakbutton.style("padding-top", "6px");
-  cancellaspeakbutton.style("padding-bottom", "6px");
-  cancellaspeakbutton.style("padding-left", "15px");
-  cancellaspeakbutton.style("padding-right", "15px");
-  cancellaspeakbutton.mouseOver(inside3).mouseOut(outside3);
-  cancellaspeakbutton.mousePressed(newRec);
-  cancellaspeakbutton.position(origineX + 222, windowHeight / 1.37);
-
-  function inside0() {
-    speakbutton.style("background-color", "#000000");
-    speakbutton.style("color", "#ffffff");
-  }
-  function outside0() {
-    speakbutton.style("background-color", "#ffffff");
-    speakbutton.style("color", "#000000");
-  }
-
-  function inside1() {
-    pausebutton.style("background-color", "#000000");
-    pausebutton.style("color", "#ffffff");
-  }
-  function outside1() {
-    pausebutton.style("background-color", "#ffffff");
-    pausebutton.style("color", "#000000");
-  }
-
-  function inside2() {
-    stopspeakbutton.style("background-color", "#000000");
-    stopspeakbutton.style("color", "#ffffff");
-  }
-  function outside2() {
-    stopspeakbutton.style("background-color", "#ffffff");
-    stopspeakbutton.style("color", "#000000");
-  }
-
-  function inside3() {
-    cancellaspeakbutton.style("background-color", "#000000");
-    cancellaspeakbutton.style("color", "#ffffff");
-  }
-  function outside3() {
-    cancellaspeakbutton.style("background-color", "#ffffff");
-    cancellaspeakbutton.style("color", "#000000");
-  }
-}
-
-function startRec() {
-  recordingAvviato = true;
-  console.log("Hai cliccato raccontami, inizia a raccontare...");
-
-  // Crea un'oggetto Speech Recognition con una callback
-  speechRec = new p5.SpeechRec("it-IT", gotSpeech);
-
-  // "Continuous recognition"
-  let continuous = true;
-  // Recognition parziale (più veloce, meno accurata)
-  let interimResults = false;
-
-  speechRec.start(continuous, interimResults);
-
-  // Evento Speech recognized
-  function gotSpeech() {
-    //console.log(speechRec);
-
-    if (speechRec.resultValue && recordingAvviato == true) {
-      racconto = racconto + speechRec.resultString + " "; // Salvo il racconto in una variabile
-      console.log("Racconto: " + racconto);
-    }
-  }
 }
 
 function pauseRec() {
@@ -344,8 +226,8 @@ function salvaimmagine(img) {
   immagini.push(img);
   // crea un numero random per positions
   // tra -randomDist e randomDist
-  randomX.push(random() * 2 * randomDist - randomDist - 15); // origineX - 15 perchè l'ho spostato al centro
-  randomY.push(random() * 2 * randomDist - randomDist + origineY - 100); // ho aggiunto + windowHeight / 2 - video.height / 2 - 100 perchè il video parte a tot px sulla y
+  randomX.push(random() * 2 * randomDist - randomDist - 10); // origineX - 10 perchè l'ho spostato al centro
+  randomY.push(random() * 2 * randomDist - randomDist - 10); 
 
   console.log("Contatore entità: " + contatoreEntita);
   console.log("Contatore immagini: " + immagini.length);
@@ -378,7 +260,7 @@ function gotResults(err, result) {
   textAlign(LEFT, TOP);
   let titolo = "metamorfosi";
   textFont(myFont2, 34);
-  text(titolo, origineX, 105);
+  text(titolo, origineX, origineY - 170);
 
   // istruzioni
   textAlign(LEFT, TOP);
@@ -386,7 +268,7 @@ function gotResults(err, result) {
   text(
     "Premi il pulsante start e inizia a raccontare qualcosa. Premi il pulsante stop quando vuoi analizzare quello che hai detto. Premi il pulsante pausa quando devi pensare. Premi il pulsante new quando vuoi analizzare una nuova frase.",
     origineX,
-    165,
+    origineY - 90,
     video.width
   );
 
@@ -394,16 +276,11 @@ function gotResults(err, result) {
   fill(255);
   strokeWeight(1);
   stroke(0);
-  rect(
-    origineX,
-    windowHeight / 2 + video.height / 2 + 4 - 100,
-    video.width,
-    90
-  );
+  rect(origineX, origineY + video.height + 4, video.width, 90);
 
   // video
   // console.log("larghezza video: " + video.width);
-  image(video, origineX, windowHeight / 2 - video.height / 2 - 100);
+  image(video, origineX, origineY, video.width, video.height);
   filter(GRAY);
 
   if (detections) {
@@ -423,18 +300,18 @@ function gotResults(err, result) {
         noStroke();
         textFont(myFont, 17);
         textAlign(LEFT, TOP);
-        text(`${item}`, origineX + 30, idx * 23 + 283);
+        text(`${item}`, origineX + 30, idx * 23 + origineY + 30);
 
         // funzione che prende il valore
         val[idx] = expressions[item];
         let valore = map(expressions[item], 0, 1, 0, 200);
         fill(0);
         noStroke();
-        rect(origineX + 150, idx * 23 + 292, valore, 6); // x y w h
+        rect(origineX + 150, idx * 23 + origineY + 38, valore, 6); // x y w h
         stroke(0);
         strokeWeight(1);
         noFill();
-        rect(origineX + 150, idx * 23 + 292, 200, 6);
+        rect(origineX + 150, idx * 23 + origineY + 38, 200, 6);
       });
 
       valoreMassimo = max(val);
@@ -484,6 +361,128 @@ function gotResults(err, result) {
   faceapi.detect(gotResults);
 }
 
+function disegnapulsanti() {
+  // bottoni
+  speakbutton = createButton("start");
+  speakbutton.style("background-color", "#ffffff");
+  speakbutton.style("color", "#000000");
+  speakbutton.style("border", "1px solid #000000");
+  speakbutton.style("font-family", "Inter-Regular");
+  speakbutton.style("font-size", "1em");
+  speakbutton.style("padding-top", "6px");
+  speakbutton.style("padding-bottom", "6px");
+  speakbutton.style("padding-left", "15px");
+  speakbutton.style("padding-right", "15px");
+  speakbutton.mouseOver(inside0).mouseOut(outside0);
+  speakbutton.mousePressed(startRec);
+  speakbutton.position(origineX + 7, origineY + video.height * 3.2 + 125);
+
+  pausebutton = createButton("pause");
+  pausebutton.style("background-color", "#ffffff");
+  pausebutton.style("color", "#000000");
+  pausebutton.style("border", "1px solid #000000");
+  pausebutton.style("font-family", "Inter-Regular");
+  pausebutton.style("font-size", "1em");
+  pausebutton.style("padding-top", "6px");
+  pausebutton.style("padding-bottom", "6px");
+  pausebutton.style("padding-left", "15px");
+  pausebutton.style("padding-right", "15px");
+  pausebutton.mouseOver(inside1).mouseOut(outside1);
+  pausebutton.mousePressed(pauseRec);
+  pausebutton.position(origineX + 77, origineY + video.height * 3.2 + 125);
+
+  stopspeakbutton = createButton("stop");
+  stopspeakbutton.style("background-color", "#ffffff");
+  stopspeakbutton.style("color", "#000000");
+  stopspeakbutton.style("border", "1px solid #000000");
+  stopspeakbutton.style("font-family", "Inter-Regular");
+  stopspeakbutton.style("font-size", "1em");
+  stopspeakbutton.style("padding-top", "6px");
+  stopspeakbutton.style("padding-bottom", "6px");
+  stopspeakbutton.style("padding-left", "15px");
+  stopspeakbutton.style("padding-right", "15px");
+  stopspeakbutton.mouseOver(inside2).mouseOut(outside2);
+  stopspeakbutton.mousePressed(stopRec);
+  stopspeakbutton.position(origineX + 157, origineY + video.height * 3.2 + 125);
+
+  cancellaspeakbutton = createButton("new");
+  cancellaspeakbutton.style("background-color", "#ffffff");
+  cancellaspeakbutton.style("color", "#000000");
+  cancellaspeakbutton.style("border", "1px solid #000000");
+  cancellaspeakbutton.style("font-family", "Inter-Regular");
+  cancellaspeakbutton.style("font-size", "1em");
+  cancellaspeakbutton.style("padding-top", "6px");
+  cancellaspeakbutton.style("padding-bottom", "6px");
+  cancellaspeakbutton.style("padding-left", "15px");
+  cancellaspeakbutton.style("padding-right", "15px");
+  cancellaspeakbutton.mouseOver(inside3).mouseOut(outside3);
+  cancellaspeakbutton.mousePressed(newRec);
+  cancellaspeakbutton.position(
+    origineX + 225,
+    origineY + video.height * 3.2 + 125
+  );
+
+  function inside0() {
+    speakbutton.style("background-color", "#000000");
+    speakbutton.style("color", "#ffffff");
+  }
+  function outside0() {
+    speakbutton.style("background-color", "#ffffff");
+    speakbutton.style("color", "#000000");
+  }
+
+  function inside1() {
+    pausebutton.style("background-color", "#000000");
+    pausebutton.style("color", "#ffffff");
+  }
+  function outside1() {
+    pausebutton.style("background-color", "#ffffff");
+    pausebutton.style("color", "#000000");
+  }
+
+  function inside2() {
+    stopspeakbutton.style("background-color", "#000000");
+    stopspeakbutton.style("color", "#ffffff");
+  }
+  function outside2() {
+    stopspeakbutton.style("background-color", "#ffffff");
+    stopspeakbutton.style("color", "#000000");
+  }
+
+  function inside3() {
+    cancellaspeakbutton.style("background-color", "#000000");
+    cancellaspeakbutton.style("color", "#ffffff");
+  }
+  function outside3() {
+    cancellaspeakbutton.style("background-color", "#ffffff");
+    cancellaspeakbutton.style("color", "#000000");
+  }
+}
+
+function startRec() {
+  recordingAvviato = true;
+  console.log("Hai cliccato raccontami, inizia a raccontare...");
+
+  // Crea un'oggetto Speech Recognition con una callback
+  speechRec = new p5.SpeechRec("it-IT", gotSpeech);
+
+  // "Continuous recognition"
+  let continuous = true;
+  // Recognition parziale (più veloce, meno accurata)
+  let interimResults = false;
+
+  speechRec.start(continuous, interimResults);
+
+  // Evento Speech recognized
+  function gotSpeech() {
+    //console.log(speechRec);
+
+    if (speechRec.resultValue && recordingAvviato == true) {
+      racconto = racconto + speechRec.resultString + " "; // Salvo il racconto in una variabile
+      console.log("Racconto: " + racconto);
+    }
+  }
+}
 function drawLandmarks(detections) {
   //console.log("detections: "+ JSON.stringify(detections))
 
@@ -507,8 +506,8 @@ function drawLandmarks(detections) {
       textAlign(CENTER, CENTER);
       text(
         racconto,
-        windowWidth / 2 - 250 + 15,
-        windowHeight / 2 + video.height / 2 - 100, // centro verticalmente rispetto rettangolo
+        windowWidth / 2 - 250 - 15,
+        origineY + video.height, // centro verticalmente rispetto rettangolo
         500,
         90
       );
@@ -520,8 +519,8 @@ function drawLandmarks(detections) {
       textAlign(CENTER, CENTER);
       text(
         racconto,
-        windowWidth / 2 - 250 + 15,
-        windowHeight / 2 + video.height / 2 - 100,
+        windowWidth / 2 - 250 - 10,
+        origineY + video.height,
         500,
         90
       );
@@ -539,7 +538,7 @@ function drawLandmarks(detections) {
     image(
       immagini[num],
       feature[3]._x + randomX[num] + origineX - wImg / 2,
-      feature[3]._y + randomY[num] - hImg / 2,
+      feature[3]._y + randomY[num] + origineY - hImg / 2,
       wImg,
       hImg
     );
@@ -548,12 +547,13 @@ function drawLandmarks(detections) {
 
 // per il resize della canvas
 function windowResized() {
-  resizeCanvas(windowWidth - 30, windowHeight);
-  origineX = windowWidth / 2 - video.width / 2;
+  resizeCanvas(windowWidth - 20, windowHeight-20);
+  origineX = windowWidth / 2 - video.width / 2 - 10;
   origineY = windowHeight / 2 - video.height / 2;
+  background(255);
 
-  speakbutton.position(origineX + 5, windowHeight / 1.37);
-  pausebutton.position(origineX + 75, windowHeight / 1.37);
-  stopspeakbutton.position(origineX + 155, windowHeight / 1.37);
-  cancellaspeakbutton.position(origineX + 222, windowHeight / 1.37);
+  speakbutton.position(origineX + 7, origineY + video.height + 125);
+  pausebutton.position(origineX + 77, origineY + video.height + 125);
+  stopspeakbutton.position(origineX + 157, origineY + video.height + 125);
+  cancellaspeakbutton.position(origineX + 225, origineY + video.height + 125);
 }
