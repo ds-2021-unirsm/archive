71fd8ac17e5b965e3d8e9bc0021d334a4ab9cfca
Graphein0707 <76476654+Graphein0707@users.noreply.github.com>
2021-06-11 22:50:39 +0200

    Parola sbagliata

--- Graphein0707/P5.js/Camminatore_incontri-scontri/sketch.js
+++ Graphein0707/P5.js/Camminatore_incontri-scontri/sketch.js
@@ -1,5 +1,5 @@
 // -
-// Camminatori incontri-scontri 0.1 by GaiaAndruccioli [Camminatori, incontri-scontri]
+// Camminatore incontri-scontri 0.1 by GaiaAndruccioli [Camminatore, incontri-scontri]
 // 2021 © GaiaAndruccioli, Daniele @Fupete, Irene @iretrtr and the course DS-2021 at DESIGN.unirsm,
 // github.com/ds-2021-unirsm — github.com/fupete - github.com/Graphein0707
 // Educational purposes, MIT License, 2021, San Marino
