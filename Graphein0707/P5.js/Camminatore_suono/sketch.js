4f5ed59409b16c84e23109c28cd92460a4619add
Graphein0707 <76476654+Graphein0707@users.noreply.github.com>
2021-06-11 23:04:35 +0200

    Update commenti iniziali e sketch

--- Graphein0707/P5.js/Camminatore_suono/sketch.js
+++ Graphein0707/P5.js/Camminatore_suono/sketch.js
@@ -1,3 +1,13 @@
+// -
+// Camminatore suono 0.1 by GaiaAndruccioli [Camminatore, suono]
+// 2021 © GaiaAndruccioli, Daniele @Fupete, Irene @iretrtr and the course DS-2021 at DESIGN.unirsm,
+// github.com/ds-2021-unirsm — github.com/fupete - github.com/Graphein0707
+// Educational purposes, MIT License, 2021, San Marino
+// —
+// Credits/Thanks to: P5 Reference sound
+// https://p5js.org/reference/#/libraries/p5.sound
+//
+
 let x, y;
 let t;
 let T;
