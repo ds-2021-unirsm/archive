3d04531a19f556fc8d3daac1b4f160086902f8ba
Graphein0707 <76476654+Graphein0707@users.noreply.github.com>
2021-06-15 15:33:03 +0200

    Update sketch.js

--- Graphein0707/P5.js/Camminatore_mondi/sketch.js
+++ Graphein0707/P5.js/Camminatore_mondi/sketch.js
@@ -1,6 +1,6 @@
 // -
-// Camminatore mondi 0.1 by GaiaAndruccioli [Camminatori, mondi]
-// 2021 © GaiaAndruccioli, Daniele @Fupete, Irene @iretrtr and the course DS-2021 at DESIGN.unirsm,
+// Camminatore mondi 0.2 by Gaia Andruccioli [Camminatori, mondi]
+// 2021 © Gaia Andruccioli, Daniele @Fupete, Irene @iretrtr and the course DS-2021 at DESIGN.unirsm,
 // github.com/ds-2021-unirsm — github.com/fupete - github.com/Graphein0707
 // Educational purposes, MIT License, 2021, San Marino
 // —
