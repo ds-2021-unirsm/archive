a62d31b8eedda14a3140c3013128c61abc3ac256
Graphein0707 <76476654+Graphein0707@users.noreply.github.com>
2021-06-15 15:52:39 +0200

    commento iniziale

--- Graphein0707/P5.js/Generatore_parole_casuali/sketch.js
+++ Graphein0707/P5.js/Generatore_parole_casuali/sketch.js
@@ -1,4 +1,9 @@
-//random words
+// -
+// Generatore di parole casuali 0.1 by Gaia Andruccioli [parole, casuale]
+// 2021 © Gaia Andruccioli, Daniele @Fupete, Irene @iretrtr and the course DS-2021 at DESIGN.unirsm,
+// github.com/ds-2021-unirsm — github.com/fupete - github.com/Graphein0707
+// Educational purposes, MIT License, 2021, San Marino
+// —
 
 var sillabe = ["ba", "be", "bi", "bo", "bu", "ca", "ce", "ci", "co", "cu", "da", "de", "di", "do", "du", "fa", "fe", "fi", "fo", "fu", "ga", "ge", "gi", "go", "gu", "la", "le", "li", "lo", "lu", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "pa", "pe", "pi", "po", "pu", "ra", "re", "ri", "ro", "ru", "sa", "se", "si", "so", "su", "ta", "te", "ti", "to", "tu", "va", "ve", "vi", "vo", "vu", "za", "ze", "zi", "zo", "zu"];
 
