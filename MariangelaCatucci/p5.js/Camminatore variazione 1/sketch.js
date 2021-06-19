1daaa5fec6ae6576182c156073c0cd5e4fe363b0
MariangelaCatucci <mariangelacatucci@gmail.com>
2021-06-20 00:13:44 +0200

    Update sketch.js

--- MariangelaCatucci/p5.js/Camminatore variazione 1/sketch.js
+++ MariangelaCatucci/p5.js/Camminatore variazione 1/sketch.js
@@ -1,4 +1,10 @@
-// random walker OOP
+// Camminatori con perlin noise 0.1 by Mariangela Catucci [camminatori, perlin noise]
+// 2021 © Mariangela @MariangelaCatucci, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete
+// Educational purposes, MIT License, 2021, San Marino
+//
+// —
+
 var w, h;
 var d = 300;
 var quanti = 10;
@@ -14,14 +20,12 @@ function setup() {
   for (var i=0; i<quanti; i++){
   	c.push(new Camminatore(i)); 
   }
-
 }
 
 
 function draw() {
  background(255);
 
- 
  // per tutti i camminatori chiama i diversi metodi utili 
  for (var i=0; i<quanti; i++){
 	c[i].move();
