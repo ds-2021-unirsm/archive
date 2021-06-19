3d019f70964453bbd6a06645f8106ea5021d1e58
Lucrezia604 <79698027+Lucrezia604@users.noreply.github.com>
2021-06-20 01:58:58 +0200

    Update sketch.js

--- Lucrezia604/Esercizi p5/5 Esercizi/01_Generatore_Parole/sketch.js
+++ Lucrezia604/Esercizi p5/5 Esercizi/01_Generatore_Parole/sketch.js
@@ -1,3 +1,11 @@
+// -
+// Generatore_Parole 0.1 by Lucrezia Nediani [Parole, Random]
+// 2021 © Lucrezia Nediani, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/Lucrezia604 — github.com/fupete
+// Educational purposes, MIT License, 2021, San Marino
+// —
+
+
 var consonanti = ["b", "c", "d", "f", "g", "h", "l", "m", "n", "p", "q", "r", "s", "t", "v", "z"];
   var vocali = ["a", "e", "i", "o", "u"];
   var estrazione;
