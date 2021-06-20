5355493e30a6f505eb49afff482eea84512e4e6f
alessiavalgimigli <79698172+alessiavalgimigli@users.noreply.github.com>
2021-06-20 09:53:18 +0100

    modifica indipendenti

--- alessiavalgimigli/Esercizi p5js/Walkers/Camminatori indipendenti/sketch.js
+++ alessiavalgimigli/Esercizi p5js/Walkers/Camminatori indipendenti/sketch.js
@@ -1,58 +1,68 @@
+// -
+// Earthquake 0.1 by Alessia Valgimigli [walkers, random]
+// 2021 © Alessia Valgimigli, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm 
+// github.com/ds-2021-unirsm — github.com/fupete
+// Educational purposes, MIT License, 2021, San Marino
+// —
+//
+// Help:
+// [mouse] pressed : earthquake
+//
+// —
+
+
 let walkers = [];
-let num = 10;
+let num = 40;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  print("se si tiene premuto sulla canvas i camminatori si sovrappongono in corrispondenza delle coordinate del mouse");
   
-  for (let a=0; a<num; a++) {
+  for (let a = 0; a < num; a++) {
     walkers.push(new Walker(a));
-  }  
+  }
 }
 
 function draw() {
-  background(255);  
-  
-  for (let b=0; b<walkers.length; b++) {
+  background(255);
+
+  for (let b = 0; b < walkers.length; b++) {
     walkers[b].display();
-    walkers[b].move();    
+    walkers[b].move();
   }
 }
 
 function Walker(_id) {
-  this.id=_id;
-  this.x=0;
-  this.y=0;
+  this.id = _id;
+  this.x = 0;
+  this.y = 0;
   //t è un numero casuale perchè se no i camminatori partirebbero tutti dallo stesso punto
-  this.t=random(100);
+  this.t = random(100);
   //creo un incremento per variare la direzione dei camminatori e renderli indipendenti
   this.tInc = random(1, 5) * 0.001;
-  
-  //genero in maniera casuale il colore dello stroke
-  this.r=random(255);
-  this.g=random(255);
-  this.b=random(255);
-  this.trasp=random(80, 100);
 
-  this.move = function(){
-    this.x=noise(this.t)*width;
-    this.y=noise(this.t+5)*height;
-    this.t += this.tInc;    
-    
-    //se si tiene premuto il mouse i camminatori si posizioneranno al centro delle coordinate del mouse
-    if(mouseIsPressed) {
-    this.x = mouseX + noise(this.t);
-    this.y = mouseY + noise(this.t+25);
+  //genero in maniera casuale il colore dello stroke
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+  this.trasp = random(80, 100);
+
+  this.move = function () {
+    this.x = noise(this.t) * width;
+    this.y = noise(this.t + 5) * height;
     this.t += this.tInc;
+
+    if (mouseIsPressed) {
+      this.x += random() * 20;
+      this.y += random() * 20;
     }
   }
-  
-  this.display = function(){
-    this.size=100*noise(this.t*3)
+
+  this.display = function () {
+    this.size = 100 * noise(this.t * 3);
     stroke(this.r, this.g, this.b, this.trasp);
     strokeWeight(15);
-    noFill;
+    noFill();
     ellipse(this.x, this.y, this.size);
-  }
+  };
 }
