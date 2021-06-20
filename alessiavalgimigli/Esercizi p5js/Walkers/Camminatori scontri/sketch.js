99219d0d790c0e39ecd46c48180046859dfbc963
alessiavalgimigli <79698172+alessiavalgimigli@users.noreply.github.com>
2021-06-20 10:21:20 +0100

    aggiornamento camminatori scontri

--- alessiavalgimigli/Esercizi p5js/Walkers/Camminatori scontri/sketch.js
+++ alessiavalgimigli/Esercizi p5js/Walkers/Camminatori scontri/sketch.js
@@ -1,7 +1,13 @@
+// -
+// Scontri sonori 0.1 by Alessia Valgimigli [walkers, sound]
+// 2021 © Alessia Valgimigli, Daniele @Fupete and the course DS-2021 at DESIGN.unirsm
+// github.com/ds-2021-unirsm — github.com/fupete
+// Educational purposes, MIT License, 2021, San Marino
+// —
+
 let camminatore = [];
 let position;
 let num = 10;
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,11 +16,12 @@ function setup() {
     //camminatori disposti casualmente nella canvas
     camminatore[i] = new Walker(random(width), random(height));
   }
+
+  monoSynth = new p5.MonoSynth();
 }
 
-
 function draw() {
-  background(0);
+  background(0, 60);
   for (var i = 0; i < camminatore.length; i++) {
     camminatore[i].move();
     camminatore[i].display();
@@ -26,61 +33,76 @@ function draw() {
       if (i != j && camminatore[i].scontro(camminatore[j])) {
         camminatore[i].cambiaColore();
         camminatore[i].cambiaForma();
+        camminatore[i].cambiaRaggio();
+        camminatore[i].sound();
         camminatore[j].cambiaColore();
         camminatore[j].cambiaForma();
+        camminatore[j].cambiaRaggio();
       }
     }
   }
 }
 
-
-
 function Walker(x, y) {
-  // COSTRUTTORE --------------------  
   this.position = createVector(100, 100);
-  this.position.x = x
-  this.position.y = y
-  this.r = 15 //raggio
+  this.position.x = x;
+  this.position.y = y;
+  this.r = 15; //raggio
   this.col = color(155); //colore di partenza
   this.t = random(100);
   this.tIncr = random(20);
 
-  // FUNZIONE COLORE -------------------- 
-  this.cambiaColore = function() {
-    this.col = color(random(255), random(255), random(255));
-  }
+  this.display = function () {
+    noStroke();
+    fill(this.col);
+    ellipse(this.position.x, this.position.y, this.r * 2, this.r * 2);
+  };
 
-  // FUNZIONE FORMA -------------------- 
-  this.cambiaForma = function() {
-    rect(this.position.x, this.position.y, this.r, this.r);
-    fill(random(255));
-  }
+  this.move = function () {
+    this.position.x = noise(this.t + this.tIncr) * width;
+    this.position.y = noise(this.t + this.tIncr + 20) * height;
+    this.t += 0.01;
+  };
 
-  // FUNZIONE SCONTRO-------------------- 
-  this.scontro = function(other) {
+  this.scontro = function (other) {
     //dist calcola la distanza tra due punti
-    this.d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+    this.d = dist(
+      this.position.x,
+      this.position.y,
+      other.position.x,
+      other.position.y
+    );
     //se la distanza è minore della somma dei due raggi vuol dire che c'è uno scontro
     if (this.d < this.r + other.r) {
       return true;
     } else {
       return false;
     }
-  }
+  };
 
-  // FUNZIONE DISPLAY-------------------- 
-  this.display = function() {
-    noStroke()
-    fill(this.col)
-    ellipse(this.position.x, this.position.y, this.r * 2, this.r * 2)
-  }
+  // FUNZIONI CAMBIAMENTO SCONTRO
+  this.cambiaColore = function () {
+    this.col = color(random(255), random(255), random(255));
+  };
 
+  this.cambiaRaggio = function () {
+    this.r = noise(this.t) * 10;
+  };
 
-  // FUNZIONE MOVIMENTO-------------------- 
-  this.move = function() {
-    this.position.x = noise(this.t + this.tIncr) * width;
-    this.position.y = noise(this.t + this.tIncr + 20) * height;
-    this.t += 0.01;
+  this.cambiaForma = function () {
+    rect(this.position.x, this.position.y, this.r, this.r);
+    fill(random(255));
+  };
 
-  }
+  this.sound = function playSynth() {
+    userStartAudio();
+    let note = random(["Fb4", "G4", "D4", "C4"]);
+    // note velocity (volume, from 0 to 1)
+    let velocity = random();
+    // time from now (in seconds)
+    let time = 0;
+    // note duration (in seconds)
+    let dur = 1 / 6;
+    monoSynth.play(note, velocity, time, dur);
+  };
 }
